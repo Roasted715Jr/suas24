ba365b068cfcf4677845558c972bedd8829ce772
Matthew Duphily <matthew.duphily@live.com>
2024-03-09 13:34:58 -0500

    Can change into guided mode after wp mission

---
+++ mavlink_test.py
@@ -0,0 +1,52 @@
+from pymavlink import mavutil
+from util import mavlink_commands
+from time import sleep
+
+using_sitl = True
+
+if using_sitl:
+    # conn = mavutil.mavlink_connection("udpin:localhost:14550", autoreconnect=False)
+    conn = mavutil.mavlink_connection("udpin:localhost:15550", autoreconnect=False)
+else:
+    conn = mavutil.mavlink_connection("/dev/ttyS14", baud=57600, autoreconnect=False)
+
+# Wait for the first heartbeat
+#   This sets the system and component ID of remote system for the link
+print("Waiting for heartbeat")
+conn.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)" % (conn.target_system, conn.target_component))
+
+print("Sending heartbeat")
+conn.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER,
+                                                mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
+
+mavlink_commands.change_mode(conn, "GUIDED")
+
+print(mavlink_commands.arm(conn))
+print("Armed")
+
+conn.mav.command_long_send(conn.target_system, conn.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF_LOCAL, 0,
+                            0,
+                            1, 0, 0, 0, 0, 0)
+
+# mavlink_commands.change_mode(conn, "AUTO")
+
+# # mavlink_commands.change_mode(conn, mavutil.mavlink.MAV_MODE_GUIDED_DISARMED)
+
+# while (True):
+#     # print(conn.recv_match(type="MISSION_CURRENT", blocking=True))
+
+#     mission_state = conn.recv_match(type="MISSION_CURRENT", blocking=True)
+#     print(mission_state)
+
+#     if mission_state.seq == mission_state.total:
+#         print("Finished mission")
+#         break
+
+# mavlink_commands.change_mode(conn, "GUIDED")
+
+# sleep(1)
+
+# print("Disarming")
+# mavlink_commands.disarm(conn)
+# print("Disarmed")
---
+++ util/mavlink_commands.py
@@ -0,0 +1,37 @@
+from pymavlink import mavutil
+
+armed = False
+
+def arm(conn):
+	print("Arming")
+	conn.mav.command_long_send(conn.target_system, conn.target_component, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1,
+								0, 0, 0, 0, 0, 0)
+	
+	response = conn.recv_match("COMMAND_ACK", blocking=True)
+	while not response or response.command != mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM:
+		continue
+
+	return response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED
+
+def disarm(conn):
+	conn.mav.command_long_send(conn.target_system, conn.target_component, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 0,
+								0, 0, 0, 0, 0, 0)
+
+	#There's an error reading the response from this
+	# response = conn.recv_match("COMMAND_ACK", blocking=True)
+	# while not response or response.command != mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM:
+	# 	continue
+
+	# return response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED
+
+def change_mode(conn, mode: str):
+	# conn.mav.command_long_send(conn.target_system, conn.target_component, mavutil.mavlink.MAV_CMD_DO_SET_MODE, mode, 0, 0,
+    #                         	0, 0, 0, 0, 0)
+	print("Changing mode to " + mode)
+	conn.set_mode(conn.mode_mapping()[mode])
+
+	# response = conn.recv_match("COMMAND_ACK", blocking=True)
+	# while not response or response.command != mode:
+	# 	continue
+
+	# return response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED
