bc658b261e469325b93b6954ef146a182bc90515
Matthew Duphily <matthew.duphily@live.com>
2024-03-26 21:08:06 -0400

    Program uploads geofence

---
+++ geofence.py
@@ -0,0 +1,30 @@
+from pymavlink import mavutil
+from pymavlink import mavwp
+from util import connection
+from util.mavlink_commands import *
+from time import sleep
+import time
+import json
+from ctypes import c_uint8, c_uint16, c_int32
+
+conn = connection.connect()
+
+# Wait for the first heartbeat
+#   This sets the system and component ID of remote system for the link
+print("Waiting for heartbeat")
+conn.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)" % (conn.target_system, conn.target_component))
+
+print("Sending heartbeat")
+conn.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER, mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
+
+mission = json.load(open("Missions/competition.json"))
+
+print(mission["geofence"])
+send_poly_geofence(conn, mission["geofence"])
+
+# print("Moving servo")
+# conn.mav.command_long_send(conn.target_system, conn.target_component, mavutil.mavlink.MAV_CMD_DO_SET_SERVO, 0,
+#                             9, 1500, 0, 0, 0, 0, 0)
+
+# sleep(3)
--- util/mavlink_commands.py
+++ util/mavlink_commands.py
@@ -28,7 +28,7 @@ def disarm(conn):
 
 def change_mode(conn, mode: str):
 	# conn.mav.command_long_send(conn.target_system, conn.target_component, mavutil.mavlink.MAV_CMD_DO_SET_MODE, mode, 0, 0,
-    #                         	0, 0, 0, 0, 0)
+	#                         	0, 0, 0, 0, 0)
 	print("Changing mode to " + mode)
 	conn.set_mode(conn.mode_mapping()[mode])
 
@@ -40,8 +40,61 @@ def change_mode(conn, mode: str):
 
 def encode_mission_item(conn, cmd: c_uint16, seq: c_uint16, param1: float, param2: float, param3: float, param4: float, x: c_int32, y: c_int32, z: float, mission_type: c_uint8 = mavutil.mavlink.MAV_MISSION_TYPE_MISSION, frame: c_uint8 = mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT):
 	return conn.mav.mission_item_int_encode(conn.target_system, conn.target_component,
-                           seq, frame, cmd,
+						   seq, frame, cmd,
 						   0, 1, #Current, autocontinue
-                           param1, param2, param3, param4,
-                           x, y, z,
-                           mission_type)
+						   param1, param2, param3, param4,
+						   x, y, z,
+						   mission_type)
+
+def enable_alt_geofence(connection):
+	#pranav
+
+	# not sure how to set the actual altitude for the fence
+
+	connection.mav.command_long_send(connection.target_system, connection.target_component, 
+									 mavutil.mavlink.MAV_CMD_DO_FENCE_ENABLE, 1, 1, 0, 0, 0, 0, 0)
+	
+	print("altitude fence enabled")
+
+def send_poly_geofence(conn, points):
+	conn.mav.mission_clear_all_send(conn.target_system, conn.target_component, mavutil.mavlink.MAV_MISSION_TYPE_FENCE)
+	conn.mav.mission_count_send(conn.target_system, conn.target_component, len(points), mavutil.mavlink.MAV_MISSION_TYPE_FENCE)
+
+	for i in range(len(points)):
+		msg = conn.recv_match(type=['MISSION_REQUEST'],blocking=True)
+		print(msg)
+		point = points[i]
+		print(f"Sending point {i}")
+		conn.mav.send(encode_mission_item(conn, mavutil.mavlink.MAV_CMD_NAV_FENCE_POLYGON_VERTEX_INCLUSION, i,
+									len(points), 0, 0, 0, int(point[0] * 1e7), int(point[1] * 1e7), 0, mission_type = mavutil.mavlink.MAV_MISSION_TYPE_FENCE))
+
+
+def enable_poly_geofence(connection):
+	#pranav
+
+	# not sure how to set the actual geofences
+	# should I get the fence coordinates from a txt file or add them in a list locally
+
+	connection.mav.command_long_send(connection.target_system, connection.target_component, 
+									 mavutil.mavlink.MAV_CMD_DO_FENCE_ENABLE, 1, 4, 0, 0, 0, 0, 0)
+	
+	# connection.mav.command_int_send(connection.target_system, connection.target_component, 
+	#                                 mavutil.mavlink.MAV_CMD_NAV_FENCE_POLYGON_VERTEX_INCLUSION, 5, geofence, 0, 0, 0, 0, 0)
+	print("enabled polygon fence")
+
+
+
+# disabling geofences should be straightforward
+
+def disable_alt_geofence(connection):
+	#pranav
+	connection.mav.command_long_send(connection.target_system, connection.target_component, 
+									 mavutil.mavlink.MAV_CMD_DO_FENCE_ENABLE, 0, 1, 0, 0, 0, 0, 0)
+	print("disabled altitude fence")
+
+
+def disable_poly_geofence(connection):
+	#pranav
+	connection.mav.command_long_send(connection.target_system, connection.target_component, 
+									 mavutil.mavlink.MAV_CMD_DO_FENCE_ENABLE, 0, 4, 0, 0, 0, 0, 0)
+	print("disabled polygon fence")
